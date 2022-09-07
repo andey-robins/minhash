ae3465a55c0714bf6cec874348dfb80ee77118bf
Andey Robins <jtuttle5@uwyo.edu>
2022-09-06 19:26:15 -0600

    Add CLI frame

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ A version of the minhash algorithm for the Dynamic Big Data Course at Uwyo
 ## Dependencies
 
 `go version go1.19 darwin/arm64`
+
+## Running
+
+`go run main.go --help`
+
+## Building
+
+`go build main.go`
--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("Run with -help for help information.")
+	}
+
 	var fPrime int
-	flag.IntVar(&fPrime, "L", 2003, "the value of L in S = {0, 1, 2, 3, ... L-1}")
+	var help, class bool
+	flag.IntVar(&fPrime, "L", 11, "the value of L in S = {0, 1, 2, 3, ... L-1}")
+	flag.BoolVar(&help, "help", false, "print help information")
+	flag.BoolVar(&class, "class", false, "set values to default for class example")
 	flag.Parse()
+
+	if help {
+		pad := func() {
+			fmt.Printf("\n\n")
+		}
+		pad()
+		fmt.Println(" Welcome to the minhash explorer tool!")
+		fmt.Print(" This code is licensed under GPLv3")
+		pad()
+		fmt.Println(" Args:")
+		fmt.Println("  -L       The upper bound in defining S = {0, 1, 2, ..., L-1}")
+		fmt.Println("  -class   Automatically set defaults to the class example")
+		fmt.Println("  -help    Display this help text :)")
+		pad()
+	}
 }
