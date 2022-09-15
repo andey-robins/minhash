a7f894041e06b125933ee5eab8b8ac5532051f67
Andey Robins <jtuttle5@uwyo.edu>
2022-09-15 17:34:44 -0600

    Add analysis tools

--- run_many.sh
+++ run_many.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+go build -o main
+mv res.txt res.txt.bak
 for i in {2..1000}
 do
     echo $i
-    go run main.go -L 10007 -Q $i -seed 11 >> res.txt
+    ./main -L 10007 -Q $i -seed 22 >> res.txt
 done
+rm main
